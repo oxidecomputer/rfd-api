c4257ce21449c32c6dd9737bda3a2161e7ebe83d
Augustus Mayo <gusmayo@gmail.com>
2024-08-20 15:52:37 -0500

    Fix license header

--- rfd-model/src/schema.rs
+++ rfd-model/src/schema.rs
@@ -1,4 +1,6 @@
-// @generated automatically by Diesel CLI.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 pub mod sql_types {
     #[derive(diesel::sql_types::SqlType)]
