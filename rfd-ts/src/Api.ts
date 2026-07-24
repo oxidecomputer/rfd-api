92a4ae9886a472831cf652f783e2263df4b816d9
Peter Tripp <peter@oxidecomputer.com>
2026-07-24 09:17:40 -0400

    Bump to v0.15.3

--- rfd-ts/src/Api.ts
+++ rfd-ts/src/Api.ts
@@ -944,7 +944,7 @@ export class Api {
    * Pulled from info.version in the OpenAPI schema. Sent in the
    * `api-version` header on all requests.
    */
-  apiVersion = '0.15.2'
+  apiVersion = '0.15.3'
 
   constructor({ host = '', baseParams = {}, token }: ApiConfig = {}) {
     this.host = host
