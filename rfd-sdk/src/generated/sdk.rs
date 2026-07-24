c5c58dabd9acbfd87cf25761a8c357ada3bebe65
Peter Tripp <peter@oxidecomputer.com>
2026-07-24 09:18:01 -0400

    Bump to v0.15.3 (#498)

--- rfd-sdk/src/generated/sdk.rs
+++ rfd-sdk/src/generated/sdk.rs
@@ -14053,7 +14053,7 @@ pub mod types {
 ///
 /// Programmatic access to RFDs
 ///
-/// Version: 0.15.2
+/// Version: 0.15.3
 pub struct Client {
     pub(crate) baseurl: String,
     pub(crate) client: reqwest::Client,
@@ -14094,7 +14094,7 @@ impl Client {
 
 impl ClientInfo<()> for Client {
     fn api_version() -> &'static str {
-        "0.15.2"
+        "0.15.3"
     }
 
     fn baseurl(&self) -> &str {
